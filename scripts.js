185f923e7034f6d6efba3a1e9d9833ab6b2fd910
Boris Serdiuk <serdiuk@amazon.com>
2017-12-15 03:00:13 +0100

    add zooming menu

--- scripts.js
+++ scripts.js
@@ -1,5 +1,45 @@
+function addElement(tag, className, parent) {
+    const element = document.createElement(tag);
+    element.className = className;
+    parent.appendChild(element);
+    return element;
+}
+
+class ImageView {
+    constructor() {
+        this.el = document.querySelector(".map");
+        this.fullMap = addElement("img", "map-full", this.el);
+        this.zoomMap = addElement("img", "map-zoom", this.el);
+        this.zoomMap.style.display = "none";
+        this.el.addEventListener("mouseenter", event => this.zoomMap.style.display = "");
+        this.el.addEventListener("mousemove", event => this.setZoomPosition(event));
+        this.el.addEventListener("mouseleave", event => this.zoomMap.style.display = "none");
+    }
+
+    setZoomPosition(event) {
+        const n = 4;
+        const a = 100;
+        const transformOffset = x => {
+            let res = a / (a - 2 * n) * x - n;
+            res = Math.max(0, res);
+            res = Math.min(a, res);
+            return res;
+        };
+        const pos = this.el.getBoundingClientRect();
+        const left = transformOffset((event.pageX - pos.top) / this.fullMap.offsetWidth * 100);
+        const top = transformOffset((event.pageY - pos.left) / this.fullMap.offsetHeight * 100);
+        this.zoomMap.style.objectPosition = `${left}% ${top}%`
+    }
+
+    setActiveImage(name) {
+        const src = `images/S_und_U-${name}.png`;
+        this.fullMap.src = src;
+        this.zoomMap.src = src;
+    }
+}
+
 let currentEntry;
-const mapFrame = document.querySelector('.map');
+const view = new ImageView()
 function setActive(entry) {
     if (currentEntry !== entry) {
         if (currentEntry) {
@@ -7,9 +47,8 @@ function setActive(entry) {
         }
         currentEntry = entry;
         entry.classList.add("entry-active");
-        mapFrame.src = `images/S_und_U-${entry.dataset.name}.png`
+        view.setActiveImage(entry.dataset.name);
     }
-
 }
 
 setActive(document.querySelector(".entry"));
